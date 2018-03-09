aa6f54b2c5f799802cab12df9050a14c38453ec2
An Nuyen <annuyen@gmail.com>
2018-03-09 18:08:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: {in:[true, false]}
   validates :artist_name, presence: true
 
-  with_options if :released? do |song|
+  with_options if: :released? do |song|
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: {less_than_or_equal_to: Time.now.year}
   end
